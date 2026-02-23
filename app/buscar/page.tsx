d520faa0240d0218d5efad3fd5e9360812000034
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 15:42:22 +0100

    yaaa weyy

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import SearchResultsPage from "@/components/SearchResultsPage";
-
+export const dynamic = "force-dynamic";
 export default function BuscarPage() {
   return <SearchResultsPage />;
 }
