2858d6bdcef6ee64565f4070f7dcbd13755be45f
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 16:31:04 +0100

    yaaaaa no more chin al codigo

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import SearchResultsPage from "@/components/SearchResultsPage";
 
 //Esto evita que Next.js intente prerenderizar la página
-export const dynamic = "force-dynamic";
+
 
 export default function BuscarPage() {
   return <SearchResultsPage />;
--- app/quienes-somos/page.tsx
+++ app/quienes-somos/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 
 import React, { useContext, useState } from "react";
 import { LanguageContext } from "../RootProviders";
