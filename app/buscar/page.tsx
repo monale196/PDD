f7fd18a61289c2c149bcf215ae0bd7ff7d440d90
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 17:58:49 +0100

    jodeer

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -3,7 +3,9 @@ export const dynamic = "force-dynamic";
 
 import SearchResultsPage from "@/components/SearchResultsPage";
 
-//Esto evita que Next.js
+//Esto evita que Next.js intente prerenderizar la página
+
+
 export default function BuscarPage() {
   return <SearchResultsPage />;
 }
--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // Habilitar SSR con modo standalone
+  output: 'standalone', // Esto le dice a Next.js que debe construir la aplicación para un entorno de servidor
+
+  images: {
+    unoptimized: true, // Desactiva la optimización de imágenes si usas imágenes externas o no optimizadas en Amplify
+    domains: ['newsroomcache.s3.eu-north-1.amazonaws.com'], // Añadir tu dominio de S3 para permitir cargar imágenes desde allí
+  },
+
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION || "eu-north-1",
     S3_BUCKET: process.env.S3_BUCKET || "newsroomcache",
+  },
+
+  // Otros ajustes que puedas necesitar
+  experimental: {
+    appDir: true, // Habilitar App Router (si usas Next.js 13 con el nuevo sistema de directorios de app)
   },
 };
 
