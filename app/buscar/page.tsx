2c493019871891175f141b5e74e715ed29ac8bbc
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 17:23:08 +0100

    joooo

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,10 +1,11 @@
 // app/buscar/page.tsx
 
 "use client";
+export const dynamic = "force-dynamic";
 import { Suspense } from "react";
 import SearchResultsPage from "@/components/SearchResultsPage";
 
-export const dynamic = "force-dynamic";
+
 
 export default function BuscarPage() {
   return (
