4a1b5a8b19a7c7f42e1fe840fa8453fc22a9498e
acervmon <126948783+acervmon@users.noreply.github.com>
2026-02-23 15:46:53 +0100

    ya?

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import SearchResultsPage from "@/components/SearchResultsPage";
+
+//Esto evita que Next.js intente prerenderizar la página
 export const dynamic = "force-dynamic";
+
 export default function BuscarPage() {
   return <SearchResultsPage />;
 }
+
+
