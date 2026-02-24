a9e79122850322ec9336d774ec7a385ba4154069
monale196 <monalecervantes@gmail.com>
2026-02-24 16:12:13 +0100

    changes en tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-﻿// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./context/**/*.{js,ts,jsx,tsx}",
-
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ["var(--font-inter)", "system-ui", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
