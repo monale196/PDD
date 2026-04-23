aae98d5d6bec717dcdcd2a9e7aebd69343f1d0a0
monale196 <monalecervantes@gmail.com>
2026-04-23 11:31:27 +0200

    oksyy

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,13 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 
 import React, { useState, useContext, useRef, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { LanguageContext } from "../app/RootProviders";
+import { SearchContext, LanguageContext } from "../app/RootProviders";
 import { NewsContext } from "../context/NewsContext";
 import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/navigation";
-import { SearchContext } from "@/context/SearchContext";
-
 import {
   Bars3Icon,
   HomeIcon,
